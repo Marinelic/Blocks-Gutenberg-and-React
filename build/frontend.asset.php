5ec2c62587c8dfc71afa02213031bad3da23d184
Marinelic <75620592+Marinelic@users.noreply.github.com>
2021-07-24 17:38:18 +0200

    Passing Block Data from  PHP into JavaScript/React

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '258f874603e7ffa5942465bdbd284ca8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '057e9b77c888c8175a2366d863e39aad');
--- src/frontend.js
+++ src/frontend.js
@@ -1,3 +1,4 @@
+import { func } from 'prop-types'
 import React from 'react'
 import ReactDOM  from 'react-dom'
 import "./frontend.scss"
@@ -5,14 +6,20 @@ import "./frontend.scss"
 const divsToUpdate = document.querySelectorAll(".paying-attention-update-me")
 
 divsToUpdate.forEach(function(div) {
-  ReactDOM.render(<Quiz />, div)
-  div.classList.remove("paying-attention-update-me")
+ const data = JSON.parse(div.querySelector("pre").innerHTML)
+   ReactDOM.render(<Quiz {...data} />, div)
+    div.classList.remove("paying-attention-update-me")
 })
 
-function Quiz() {
+function Quiz(props) {
   return (
    <div className="paying-attention-frontend">
-    Hello from React
+    <p>{props.question}</p>
+    <ul>
+     {props.answers.map(function(answer) {
+       return <li>{answer}</li>
+     })}
+    </ul>
    </div>
   )
 }
