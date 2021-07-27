b4dc1051b5d8473ab934ea47e4d4d41995fc6b50
Marinelic <75620592+Marinelic@users.noreply.github.com>
2021-07-27 20:32:04 +0200

    Click On (Guess) An Answer

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '057e9b77c888c8175a2366d863e39aad');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '79dd95d1fdb9f49965fa2638312c3544');
