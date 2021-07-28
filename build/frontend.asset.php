78b1cc21cfdf4da10a3486f945011c5534045922
Marinelic <75620592+Marinelic@users.noreply.github.com>
2021-07-28 16:36:13 +0200

    Attention To Detail

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '79dd95d1fdb9f49965fa2638312c3544');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '7aad18dd65dd0347b500119e8026b01d');
